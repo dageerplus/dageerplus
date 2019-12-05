a39bcec5be1c661866e7ce47da7fcf5e480a5091
linghuchong <linghuchong@qq.com>
2019-12-05 11:07:05 +0800

    见二十三

--- src/wugong/dugujiujian.java
+++ src/wugong/dugujiujian.java
@@ -1,10 +1,10 @@
-package wugong;
+ package wugong;
 
 public class dugujiujian {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("剑二十二");
 	}
 
 }
